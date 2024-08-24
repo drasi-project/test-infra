178bc81c1e3396176d8f73b5c84ebe44893f4947
Agent of Reality <agentx@agentofreality.com>
2024-08-23 23:20:22 -0700

    moving code

--- e2e-test-framework/test-source-service/src/test_repo/dataset.rs
+++ e2e-test-framework/test-source-service/src/test_repo/dataset.rs
@@ -1,10 +1,13 @@
 use std::{collections::HashMap, error::Error, path::PathBuf};
 
+use azure_storage::prelude::*;
+use azure_storage_blobs::prelude::*;
+use azure_storage_blobs::container::operations::BlobItem;
+use futures::stream::StreamExt;
 use serde::Serialize;
+use tokio::{fs::File, io::AsyncWriteExt};
 
 use crate::{mask_secret, test_script::test_script_player::TestScriptPlayerSettings};
-
-use super::download_remote_repo_folder;
 
 #[derive(Clone, Debug, Serialize)]
 pub struct DataSetSettings {
@@ -171,4 +174,107 @@ impl DataSet {
 
         Ok(file_path_map)
     }
+}
+
+
+async fn download_remote_repo_folder(
+    storage_account: String,
+    storage_access_key: String,
+    storage_container: String,
+    storage_path: String,
+    remote_repo_folder: String, 
+    local_repo_folder: PathBuf,
+) -> Result<Vec<PathBuf>, Box<dyn Error>> {
+    log::info!("Downloading Remote Repo Folder - {:?} : {:?}/{:?}", storage_account, storage_container, storage_path);
+
+    let storage_credentials = StorageCredentials::access_key(&storage_account, storage_access_key.clone());
+    let container_client = ClientBuilder::new(&storage_account, storage_credentials)
+        .container_client(&storage_container);
+
+    let mut stream = container_client
+            .list_blobs()
+            .prefix(remote_repo_folder.clone())
+            .into_stream();
+    
+    // Vector of tasks to download the files.
+    // Each task will download a single file.
+    // All downloads must be complete before returning.
+    let mut tasks = vec![];
+
+    // Vector of local file paths being downloaded.
+    let mut local_file_paths = vec![];
+
+    while let Some(result) = stream.next().await {
+        let blob_list = result?;
+        for blob_item in blob_list.blobs.items {
+            match blob_item {
+                BlobItem::Blob(blob) => {
+                    let blob_name = blob.name;
+
+                    // Create the local file path for the blob.
+                    let stripped_blob_file_name = blob_name.strip_prefix(&remote_repo_folder).unwrap();
+                    let local_file_path = local_repo_folder.clone().join(&stripped_blob_file_name);
+
+                    // Process the blob as a directory if it doesn't have an extension.
+                    if local_file_path.extension().is_none() {
+                        log::trace!("Creating directory: {:?}", local_file_path);
+                        tokio::fs::create_dir_all(local_file_path).await?;
+                    } else {
+                        // Add the local file path to the list of files being downloaded.
+                        local_file_paths.push(local_file_path.clone());
+
+                        let task = tokio::spawn(download_file(
+                            container_client.blob_client(&blob_name), 
+                            local_file_path
+                        ));
+    
+                        tasks.push(task);
+                    }
+                },
+                BlobItem::BlobPrefix(prefix) => {
+                    log::trace!("Ignoring Blob Prefix: {:?}", prefix.name);
+                }
+            }
+        }
+    }
+
+    match futures::future::try_join_all(tasks).await {
+        Ok(_) => return Ok(local_file_paths),
+        Err(e) => {
+            return Err(e.into());
+        }
+    }
+}
+
+async fn download_file(
+    blob_client: BlobClient, 
+    local_file_path: PathBuf
+) -> Result<(), Box<dyn Error + Send + Sync>> {
+    log::debug!("Downloading test script file {} to {}", blob_client.blob_name(), local_file_path.to_str().unwrap());
+
+    // Create the local file to hold the blob data.
+    let mut local_file = File::create(local_file_path).await?;
+
+    // Download the blob data.
+    let mut stream = blob_client.get().into_stream();
+
+    while let Some(value) = stream.next().await {
+
+        let mut body = value?.data;
+
+        while let Some(value) = body.next().await {
+
+            match value {
+                Ok(bytes) => {
+                    let _ = local_file.write_all(&bytes).await;
+                },
+                Err(e) => {
+                    log::error!("Error getting blob data: {}", e);
+                    return Err(e.into());
+                }
+            };
+        }
+    }
+
+    Ok(())
 }

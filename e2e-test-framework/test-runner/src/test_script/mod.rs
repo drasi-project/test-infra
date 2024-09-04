56d5a07727c96558e34ba1a3742a650a02b92585
Agent of Reality <agentx@agentofreality.com>
2024-09-03 20:12:27 -0700

    Add Display support to SourceChangeEvent struct

--- e2e-test-framework/test-runner/src/test_script/mod.rs
+++ e2e-test-framework/test-runner/src/test_script/mod.rs
@@ -4,14 +4,6 @@ pub mod change_script_reader;
 
 use serde::{Deserialize, Serialize};
 
-#[derive(Clone, Debug, Serialize, Deserialize)]
-pub struct SourceChangeEvent {
-    pub op: String,
-    pub ts_ms: u64,
-    pub schema: String,
-    pub payload: SourceChangeEventPayload,
-}
-
 type SourceChangeEventBefore = serde_json::Value; // Arbitrary JSON object for before
 type SourceChangeEventAfter = serde_json::Value; // Arbitrary JSON object for after
 
@@ -29,4 +21,28 @@ pub struct SourceChangeEventSourceInfo {
     pub ts_ms: u64,
     pub ts_sec: u64,
     pub lsn: u64,
+}
+
+#[derive(Clone, Debug, Serialize, Deserialize)]
+pub struct SourceChangeEvent {
+    pub op: String,
+    pub ts_ms: u64,
+    pub schema: String,
+    pub payload: SourceChangeEventPayload,
+}
+
+impl std::fmt::Display for SourceChangeEvent {
+    fn fmt(&self, f: &mut std::fmt::Formatter) -> std::fmt::Result {
+
+        match serde_json::to_string(self) {
+            Ok(json_data) => {
+                let json_data_unescaped = json_data
+                    .replace("\\\"", "\"") 
+                    .replace("\\'", "'"); 
+
+                write!(f, "{}", json_data_unescaped)
+            },
+            Err(e) => return write!(f, "Error serializing SourceChangeEvent: {:?}. Error: {}", self, e),
+        }
+    }
 }

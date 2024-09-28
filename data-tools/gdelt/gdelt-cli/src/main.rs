ea72ee56bc5f0feaba6e24db7d62235f7c1712c8
Agent of Reality <agentx@agentofreality.com>
2024-09-28 10:29:27 -0700

    gdelt file downloads working

--- data-tools/gdelt/gdelt-cli/src/main.rs
+++ data-tools/gdelt/gdelt-cli/src/main.rs
@@ -1,14 +1,16 @@
 use std::{collections::HashSet, hash::{Hash, Hasher}, path::PathBuf};
 
-use chrono::{NaiveDate, NaiveDateTime, NaiveTime, TimeDelta, Timelike};
+use chrono::{NaiveDate, NaiveDateTime, NaiveTime, TimeDelta, Timelike, Utc};
 use clap::{Args, Parser, Subcommand, ValueEnum};
+use reqwest::Client;
+use tokio::{fs::File, io::AsyncWriteExt};
 
 /// String constant containing the address of GDELT data on the Web
 const GDELT_DATA_URL: &str = "http://data.gdeltproject.org/gdeltv2";
 
 /// String constant representing the default GDELT data cache folder path
 /// This is the folder where GDELT data files are downloaded and stored if not provided by the user.
-const _DEFAULT_CACHE_FOLDER_PATH: &str = "./gdelt_data_cache";
+const DEFAULT_CACHE_FOLDER_PATH: &str = "./gdelt_data_cache";
 
 /// Enum representing the different types of GDELT data that can be downloaded
 #[derive(Copy, Clone, Debug, PartialEq, Eq, PartialOrd, Ord, ValueEnum)]
@@ -48,10 +50,10 @@ enum Commands {
         data_selection: DataSelectionArgs,
 
         /// A flag to indicate whether existing files should be overwritten
-        #[arg(short = 'o', long, default_value_t = true)]
+        #[arg(short = 'o', long, default_value_t = false)]
         overwrite: bool,
 
-        /// A flag to indicate whether the downloaded files should be unzipped
+        /// A flag to indicate whether the downloaded files should be unzipped automatically
         #[arg(short = 'u', long, default_value_t = true)]
         unzip: bool,
     },
@@ -99,17 +101,30 @@ struct DataSelectionArgs {
 
 #[tokio::main]
 async fn main() {
+    env_logger::init();
+
     let params = Params::parse();
 
-    match params.command {
+    let cache_folder_path = params.cache_folder_path.unwrap_or_else(|| PathBuf::from(DEFAULT_CACHE_FOLDER_PATH));
+
+    let res = match params.command {
         Commands::Get { data_selection, overwrite, unzip } => {
-            handle_get_command(data_selection, overwrite, unzip);
+            handle_get_command(data_selection, cache_folder_path, overwrite, unzip).await
         }
         Commands::Unzip { data_selection, overwrite } => {
-            handle_unzip_command(data_selection, overwrite);
+            handle_unzip_command(data_selection, cache_folder_path, overwrite).await
         }
         Commands::Load { data_selection, database_url, database_user, database_password } => {
-            handle_load_command(data_selection, database_url, database_user, database_password);
+            handle_load_command(data_selection, cache_folder_path, database_url, database_user, database_password).await
+        }
+    };
+
+    match res {
+        Ok(_) => {
+            println!("Command completed successfully");
+        }
+        Err(e) => {
+            eprintln!("gdelt command failed: {:?}", e);
         }
     }
 }
@@ -212,88 +227,132 @@ fn parse_datetime(datetime_str: &str) -> anyhow::Result<NaiveDateTime> {
     Ok(NaiveDateTime::new(date, time))
 }
 
-fn handle_get_command(data_selection: DataSelectionArgs, overwrite: bool, unzip: bool) {
-    println!("Get command:");
-    match &data_selection.file_start {
-        Some(start) => {
-            println!("  - Start: {:?}, Adjusted Start: {:?}", &start, parse_start_datetime(&start).unwrap());
-        }
-        None => println!("  - Start: None"),
-        
-    };
-    match &data_selection.file_end {
-        Some(end) => {
-            println!("  - End: {:?}, Adjusted End: {:?}", &end, parse_end_datetime(&end).unwrap());
-        }
-        None => println!("  - End: None"),
-        
-    };
-    println!("  - Data Types: {:?}", data_selection.data_type);
-    println!("  - Overwrite: {}", overwrite);
-    println!("  - Unzip: {}", unzip);
+fn get_date_range(data_selection: &DataSelectionArgs) -> anyhow::Result<(NaiveDateTime, NaiveDateTime)> {
 
-    let downloads = create_gdelt_download_list(
-        parse_start_datetime(data_selection.file_start.as_ref().unwrap()).unwrap(),
-        parse_end_datetime(data_selection.file_end.as_ref().unwrap()).unwrap(),
+    // If there is a start date and end date, parse them and validate that the end date is equal to or after the start date
+    // If there is only a start date, use the start date as both the start date and the end date
+    // If there is only an end date return an error
+    // If there is no date use the current date as both the start date and the end date
+
+    match (data_selection.file_start.as_ref(), data_selection.file_end.as_ref()) {
+        (Some(start_str), Some(end_str)) => {
+            let start_datetime = parse_start_datetime(start_str)?;
+            let end_datetime = parse_end_datetime(end_str)?;
+            if end_datetime < start_datetime {
+                anyhow::bail!("End date must be equal to or after the start date");
+            }
+            Ok((start_datetime, end_datetime))
+        }
+        (Some(start_str), None) => {
+            let start_datetime = parse_start_datetime(start_str)?;
+            Ok((start_datetime, start_datetime))
+        }
+        (None, Some(_)) => anyhow::bail!("End date provided without a start date"),
+        (None, None) => {
+            let current_datetime_str = Utc::now().naive_utc().format("%Y%m%d%H%M%S").to_string();
+            let current_datetime = parse_start_datetime(&current_datetime_str)?;
+
+            Ok((current_datetime, current_datetime))
+        },
+    }
+
+}
+
+async fn handle_get_command(data_selection: DataSelectionArgs, cache_folder_path: PathBuf, overwrite: bool, unzip: bool) -> anyhow::Result<()> {
+    log::debug!("Get command:");
+
+    let (start_datetime, end_datetime) = get_date_range(&data_selection)?;
+
+    // Display a summary of what the command is going to do based on the input parameters and the calculated date range
+    println!("Getting GDELT Data:");
+    println!("  - date range: {} to {}", &start_datetime, &end_datetime);
+    println!("  - data types: {:?}", data_selection.data_type);
+    println!("  - cache folder: {:?}", cache_folder_path);
+    println!("  - overwrite: {}", overwrite);
+
+    let downloads = create_gdelt_download_list
+        (start_datetime, 
+        end_datetime,
         data_selection.data_type.iter().cloned().collect(),
-        PathBuf::from("./gdelt_data_cache"),
+        cache_folder_path,
         overwrite,
     ).unwrap();
 
-    // Display the list of files to be downloaded
-    println!("Files to be downloaded:");
-    for (url, path) in downloads {
-        println!("  - URL: {}", url);
-        println!("  - Path: {:?}", path);
-    };
+    // Display the list of files to be downloaded, without taking ownership of the list or content
+    println!("Download Tasks:");
+    for download in &downloads {
+        println!("  - {:?}", download);
+    }
 
+    // Download the files
+    let download_results = download_gdelt_zip_files(downloads).await.unwrap();
+
+    println!("File download results:");
+    for download in &download_results {
+        println!("  - {:?}", download);
+    }
+
+    // TODO: Unzip the files if the unzip flag is set
+
+    Ok(())
 }
 
-fn handle_unzip_command(data_selection: DataSelectionArgs, overwrite: bool) {
-    println!("Unzip command:");
-    match data_selection.file_start {
-        Some(start) => {
-            println!("  - Start: {:?}, Adjusted Start: {:?}", &start, parse_start_datetime(&start).unwrap());
-        }
-        None => println!("  - Start: None"),
-        
-    };
-    match data_selection.file_end {
-        Some(end) => {
-            println!("  - End: {:?}, Adjusted End: {:?}", &end, parse_end_datetime(&end).unwrap());
-        }
-        None => println!("  - End: None"),
-        
-    };
-    println!("  - Data Types: {:?}", data_selection.data_type);
-    println!("  - Overwrite: {}", overwrite);
+async fn handle_unzip_command(data_selection: DataSelectionArgs, cache_folder_path: PathBuf, overwrite: bool) -> anyhow::Result<()> {
+    log::debug!("Unzip command:");
+
+    let (start_datetime, end_datetime) = get_date_range(&data_selection)?;
+
+    // Display a summary of what the command is going to do based on the input parameters and the calculated date range
+    println!("Unzipping GDELT Data:");
+    println!("  - date range: {} to {}", &start_datetime, &end_datetime);
+    println!("  - data types: {:?}", data_selection.data_type);
+    println!("  - cache folder: {:?}", cache_folder_path);
+    println!("  - overwrite: {}", overwrite);
+
+    Ok(())
 }
 
-fn handle_load_command(data_selection: DataSelectionArgs, database_url: Option<String>, database_user: Option<String>, database_password: Option<String>) {
-    println!("Load command:");
-    match data_selection.file_start {
-        Some(start) => {
-            println!("  - Start: {:?}, Adjusted Start: {:?}", &start, parse_start_datetime(&start).unwrap());
-        }
-        None => println!("  - Start: None"),
-        
-    };
-    match data_selection.file_end {
-        Some(end) => {
-            println!("  - End: {:?}, Adjusted End: {:?}", &end, parse_end_datetime(&end).unwrap());
-        }
-        None => println!("  - End: None"),
-        
-    };
-    println!("  - Data Types: {:?}", data_selection.data_type);
+async fn handle_load_command(data_selection: DataSelectionArgs, cache_folder_path: PathBuf, database_url: Option<String>, database_user: Option<String>, database_password: Option<String>) -> anyhow::Result<()>  {
+    log::debug!("Load command:");
+
+    let (start_datetime, end_datetime) = get_date_range(&data_selection)?;
+
+    // Display a summary of what the command is going to do based on the input parameters and the calculated date range
+    println!("Unzipping GDELT Data:");
+    println!("  - date range: {} to {}", &start_datetime, &end_datetime);
+    println!("  - data types: {:?}", data_selection.data_type);
+    println!("  - cache folder: {:?}", cache_folder_path);
     println!("  - Database URL: {:?}", database_url);
     println!("  - Database User: {:?}", database_user);
     println!("  - Database Password: {:?}", database_password);
+
+    Ok(())
 }
 
-fn create_gdelt_download_list(start_datetime: NaiveDateTime, end_datetime: NaiveDateTime, file_types: HashSet<DataType>, cache_folder_path: PathBuf, overwrite: bool) -> anyhow::Result<Vec<(String, PathBuf)>> {
+#[derive(Debug)]
+struct DownloadFileTask {
+    url: String,
+    path: PathBuf,
+    result: Option<anyhow::Result<()>>,
+}
 
-    let mut downloads: Vec<(String, PathBuf)> = Vec::new();
+impl DownloadFileTask {
+    fn new(url: String, path: PathBuf) -> Self {
+        Self {
+            url,
+            path,
+            result: None,
+        }
+    }
+
+    fn set_result(&mut self, result: anyhow::Result<()>) {
+        self.result = Some(result);
+    }
+}
+
+fn create_gdelt_download_list(start_datetime: NaiveDateTime, end_datetime: NaiveDateTime, file_types: HashSet<DataType>, cache_folder_path: PathBuf, overwrite: bool) -> anyhow::Result<Vec<DownloadFileTask>> {
+
+    let mut download_tasks: Vec<DownloadFileTask> = Vec::new();
 
     // Create the cache_folder_path if it does not exist
     if !cache_folder_path.exists() {
@@ -311,7 +370,7 @@ fn create_gdelt_download_list(start_datetime: NaiveDateTime, end_datetime: Naive
             let path = cache_folder_path.join(format!("zip/{}.export.CSV.zip", timestamp));
             if !path.exists() || overwrite {
                 let url = format!("{}/{}.export.CSV.zip", GDELT_DATA_URL, timestamp);
-                downloads.push((url, path));
+                download_tasks.push(DownloadFileTask::new(url, path));
             }
         }
 
@@ -320,7 +379,7 @@ fn create_gdelt_download_list(start_datetime: NaiveDateTime, end_datetime: Naive
             let path = cache_folder_path.join(format!("zip/{}.gkg.csv.zip", timestamp));
             if !path.exists() || overwrite {
                 let url = format!("{}/{}.gkg.csv.zip", GDELT_DATA_URL, timestamp);
-                downloads.push((url, path));
+                download_tasks.push(DownloadFileTask::new(url, path));
             }
         }
 
@@ -329,7 +388,7 @@ fn create_gdelt_download_list(start_datetime: NaiveDateTime, end_datetime: Naive
             let path = cache_folder_path.join(format!("zip/{}.mentions.CSV.zip", timestamp));
             if !path.exists() || overwrite {
                 let url = format!("{}/{}.mentions.CSV.zip", GDELT_DATA_URL, timestamp);
-                downloads.push((url, path));
+                download_tasks.push(DownloadFileTask::new(url, path));
             }
         }
 
@@ -338,11 +397,53 @@ fn create_gdelt_download_list(start_datetime: NaiveDateTime, end_datetime: Naive
     }
 
     // Return a Vec of the urls and local paths for the files to be downloaded
-    Ok(downloads)
+    Ok(download_tasks)
 }
 
-// async fn download_gdelt_zip_files(urls: Vec<String>, cache_folder_path: PathBuf) -> anyhow::Result<Vec<PathBuf>> {
-//     let mut files : Vec<PathBuf> = Vec::new();
+async fn download_gdelt_zip_files(mut download_tasks: Vec<DownloadFileTask>) -> anyhow::Result<Vec<DownloadFileTask>> {
+    let client = Client::new();
 
-// }
+    // Create a collection of futures for downloading files
+    let mut tasks = Vec::new();
 
+    for task in &mut download_tasks {
+        let url = task.url.clone();
+        let path = task.path.clone();
+        let client = client.clone();
+
+        // Spawn the download task and update the result in the DownloadFileTask
+        let fut = async move {
+            let result = download_file(client, url, path).await;
+            result
+        };
+
+        tasks.push(fut);
+    }
+
+    // Await all the tasks and update the results
+    for (i, task_result) in futures::future::join_all(tasks).await.into_iter().enumerate() {
+        download_tasks[i].set_result(task_result);
+    }
+
+    Ok(download_tasks)
+}
+
+// Helper function to download a file from the URL and save it to the given path
+async fn download_file(client: Client, url: String, path: PathBuf) -> anyhow::Result<()> {
+
+    // Make sure the parent directory exists
+    if let Some(parent) = path.parent() {
+        if !parent.exists() {
+            std::fs::create_dir_all(parent)?;
+        }
+    }
+
+    let response = client.get(&url).send().await?.error_for_status()?;
+    let mut file = File::create(&path).await?;
+    let content = response.bytes().await?;
+
+    file.write_all(&content).await?;
+    file.flush().await?;
+
+    Ok(())
+}

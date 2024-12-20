c74cb39f9a6881561797f97000a508f9abfa51f4
Agent of Reality <agentx@agentofreality.com>
2024-12-20 11:04:12 -0800

    clean DataCollector

--- e2e-test-framework/data-collector/src/lib.rs
+++ e2e-test-framework/data-collector/src/lib.rs
@@ -200,25 +200,20 @@ impl DataCollection {
 mod tests {
     use std::path::PathBuf;
 
-    use config::{RedisSourceChangeQueueReaderConfig, SourceChangeQueueReaderConfig, SourceChangeRecorderConfig};
     use test_data_store::{TestDataStore, TestDataStoreConfig};
 
     use super::*;
 
     #[tokio::test]
-    async fn data_collector_data_cache_create() -> anyhow::Result<()> {
+    async fn create_datacollector() -> anyhow::Result<()> {
         // Create a random path to use as the Data Store folder name.
         let data_store_path = format!("./tests/{}", uuid::Uuid::new_v4().to_string());
         let data_store_path_buf = PathBuf::from(&data_store_path);
 
         let data_store_config = TestDataStoreConfig {
-            data_collection_folder: None,
             data_store_path: Some(data_store_path),
-            delete_on_start: Some(true),
             delete_on_stop: Some(true),
-            test_repos: None,
-            test_repo_folder: None,
-            test_run_folder: None,
+            ..TestDataStoreConfig::default()
         };
         let test_data_store = Arc::new(TestDataStore::new(data_store_config).await?);
 
@@ -230,66 +225,68 @@ mod tests {
         // Check that the DataCollector is in the Initialized state.
         assert_eq!(data_collector.get_status().await?, DataCollectorStatus::Initialized);
 
-        // Ensure the Data Store folder was removed.
-        assert_eq!(data_store_path_buf.exists(), false);
-
-        Ok(())
-    }
-
-    #[tokio::test]
-    async fn interactive_test()  -> anyhow::Result<()> {
-        // Create a random String to use as the data store name.
-        let data_store_path = format!("tests/{}", uuid::Uuid::new_v4().to_string());
-        let data_store_path_buf = PathBuf::from(&data_store_path);
-
-        let data_store_config = TestDataStoreConfig {
-            data_collection_folder: None,
-            data_store_path: Some(data_store_path),
-            delete_on_start: Some(true),
-            delete_on_stop: Some(true),
-            test_repos: None,
-            test_repo_folder: None,
-            test_run_folder: None,
-        };
-        let test_data_store = Arc::new(TestDataStore::new(data_store_config).await?);
-
-        let config = DataCollectorConfig {
-            data_collections: vec![DataCollectionConfig {
-                id: "hello-world".to_string(),
-                queries: vec![],
-                sources: vec![DataCollectionSourceConfig {
-                    source_id: "hello-world".to_string(),
-                    bootstrap_data_recorder: None,
-                    source_change_recorder: Some(SourceChangeRecorderConfig {
-                        drain_queue_on_stop: Some(false),
-                        change_queue_reader: Some(SourceChangeQueueReaderConfig::Redis(RedisSourceChangeQueueReaderConfig {
-                            host: Some("localhost".to_string()),
-                            port: Some(6379),
-                            queue_name: Some("hello-world-change".to_string()),
-                        })),
-                        change_event_loggers: vec![],
-                    }),
-                    start_immediately: false,
-                }],
-            }]
-        };
-        let data_collector = DataCollector::new(config, test_data_store).await.unwrap();
-
-        // Check that the DataCollector is in the Initialized state.
-        assert_eq!(data_collector.get_status().await?, DataCollectorStatus::Initialized);
-
-        // Start the Source, which will start the SourceChangeRecorder
-        data_collector.start_data_collection_sources("hello-world", false, true).await.unwrap();        
-
-        println!("Press Enter to continue...");
-        let _ = std::io::stdin().read_line(&mut String::new()).unwrap();
-
-        // Stop the Source, which will stop the SourceChangeRecorder
-        // data_collector.stop_data_collection_source("hello-world", "beacon").await.unwrap();        
+        drop(data_collector);        
 
         // Ensure the Data Store folder was removed.
         assert_eq!(data_store_path_buf.exists(), false);
 
         Ok(())
     }
+
+    // #[tokio::test]
+    // async fn interactive_test()  -> anyhow::Result<()> {
+    //     // Create a random String to use as the data store name.
+    //     let data_store_path = format!("tests/{}", uuid::Uuid::new_v4().to_string());
+    //     let data_store_path_buf = PathBuf::from(&data_store_path);
+
+    //     let data_store_config = TestDataStoreConfig {
+    //         data_collection_folder: None,
+    //         data_store_path: Some(data_store_path),
+    //         delete_on_start: Some(true),
+    //         delete_on_stop: Some(true),
+    //         test_repos: None,
+    //         test_repo_folder: None,
+    //         test_run_folder: None,
+    //     };
+    //     let test_data_store = Arc::new(TestDataStore::new(data_store_config).await?);
+
+    //     let config = DataCollectorConfig {
+    //         data_collections: vec![DataCollectionConfig {
+    //             id: "hello-world".to_string(),
+    //             queries: vec![],
+    //             sources: vec![DataCollectionSourceConfig {
+    //                 source_id: "hello-world".to_string(),
+    //                 bootstrap_data_recorder: None,
+    //                 source_change_recorder: Some(SourceChangeRecorderConfig {
+    //                     drain_queue_on_stop: Some(false),
+    //                     change_queue_reader: Some(SourceChangeQueueReaderConfig::Redis(RedisSourceChangeQueueReaderConfig {
+    //                         host: Some("localhost".to_string()),
+    //                         port: Some(6379),
+    //                         queue_name: Some("hello-world-change".to_string()),
+    //                     })),
+    //                     change_event_loggers: vec![],
+    //                 }),
+    //                 start_immediately: false,
+    //             }],
+    //         }]
+    //     };
+    //     let data_collector = DataCollector::new(config, test_data_store).await.unwrap();
+
+    //     // Check that the DataCollector is in the Initialized state.
+    //     assert_eq!(data_collector.get_status().await?, DataCollectorStatus::Initialized);
+
+    //     // Start the Source, which will start the SourceChangeRecorder
+    //     data_collector.start_data_collection_sources("hello-world", false, true).await.unwrap();        
+
+    //     println!("Press Enter to continue...");
+    //     let _ = std::io::stdin().read_line(&mut String::new()).unwrap();
+
+    //     // Stop the Source, which will stop the SourceChangeRecorder
+    //     // data_collector.stop_data_collection_source("hello-world", "beacon").await.unwrap();        
+
+    //     // Ensure the Data Store folder was removed.
+    //     assert_eq!(data_store_path_buf.exists(), false);
+
+    //     Ok(())
+    // }
 }
